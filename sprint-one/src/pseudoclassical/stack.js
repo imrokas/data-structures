3d36eb601ced79db99997080f48382a4c9923ebd
imrokas <rokas@imrokas.com>
2015-11-19 14:06:26 -0500

    Implemented pseudoclassical patter on stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.currentSize = 0;
+  this.memory = {};
+};
+
+Stack.prototype.push = function(value){
+	this.currentSize++;
+	this.memory[this.currentSize] = value;
+};
+Stack.prototype.pop = function(){
+	if(this.currentSize){
+	  var poppedItem = this.memory[this.currentSize];
+	  delete this.memory[this.currentSize];
+	  this.currentSize--;
+	  return poppedItem;
+	}
+};
+Stack.prototype.size = function(){
+	return this.currentSize;
 };
 
 
