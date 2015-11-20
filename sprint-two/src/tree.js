8da69088cd3773ebfac3111173a4d453dac3721a
imrokas <rokas@imrokas.com>
2015-11-20 13:11:45 -0500

    implemented tree structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,8 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.children = [];
 
   return newTree;
 };
@@ -15,14 +15,30 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+	this.children.push(Tree(value));
 
 };
 
 treeMethods.contains = function(target){
+	if(this.value === target) {
+		return true;
+	}
 
+	for(var i = 0; i < this.children.length; i++) {
+		if(this.children[i].contains(target)) {
+			return true;
+		}
+	}
+
+	return false;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ */
+
+ /*
+ 	addChild: constant
+ 	contains: polynomial
  */
