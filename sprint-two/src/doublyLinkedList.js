4ec8f9df2024dfa52774a48f1913abe1ced6f5ad
imrokas <rokas@imrokas.com>
2015-11-21 14:39:15 -0500

    Added test file and shell for doubly linked list

---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,58 @@
+var DoublyLinkedList = function(){
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value){
+    if(!list.head){
+      list.head = Node(value);
+      list.tail = list.head;
+    }else{
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
+  };
+
+  list.removeHead = function(){
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
+  };
+
+  list.contains = function(target){
+    var tempNode = list.head;
+    while(tempNode) {
+      if(tempNode.value === target){
+        return true;
+      }
+      tempNode && (tempNode = tempNode.next);
+    }
+    return false;
+  };
+
+  list.addToHead = function(value) {
+    
+  };
+  list.removeTail = function() {};
+
+  return list;
+};
+
+var Node = function(value){
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+  node.previous = null;
+
+  return node;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
+/*
+  addToTail: constant
+  removeHead: constant
+  contains: linear
+*/
